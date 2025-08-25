aceb9e435b462565d3965fa0c79252a9547fcd6f
Muhammad Najwan <najwanhassan03@gmail.com>
2025-08-25 13:28:04 +0800

    loading animation

---
+++ loading.js
@@ -0,0 +1,4 @@
+window.addEventListener("flutter-first-frame", function() {
+          var element = document.getElementsByClassName("loading");
+          element[0].parentNode.removeChild(element[0]);
+      });
